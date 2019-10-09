2d82babeff0140c2a948e3a9590e489ddea794de
Chris Holcomb <chris@pooldashboard.com>
2019-10-09 13:24:05 -0500

    code emailed by Sammy

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
+
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
+
 using namespace std;
 
 int main() {
-
-   printf("Hello World\n");
-
-   return 0;
+    string fileName, madLibsContent;
+    ifstream madLibsFile;
+    int count = 0, x, y;
+    
+    do {
+        count ++;
+        cout << "Enter File Name: ";
+        getline(cin, fileName);
+        cout << fileName << endl;
+        
+        madLibsFile.open(fileName);
+    } while (!madLibsFile && count < 3);
+    
+    if (!madLibsFile) {
+        cout << "Error: File Failed to open after 3 tries." << endl;
+        exit(1);
+    }
+    getline(madLibsFile, madLibsContent);
+    getline(madLibsFile, madLibsContent);
+    
+    while (madLibsFile >> x >> y) {
+        size_t found = madLibsContent.find('*');
+        if (found != string::npos) {
+            
+        }
+    }
+    
+    
+    
+    
+    return 0;
 }
