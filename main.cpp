956bbf24e1d0912621884f2241d56988459d1c3c
Chris Holcomb <chris@pooldashboard.com>
2019-10-11 18:05:01 -0500

    workind sampel

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int main() {
+   string fileName, fileContent, strR, strL, strJ;
+   int intR, intL, intJ;
+   ifstream inputFile;
 
-   printf("Hello World\n");
+   cout << "Enter file name: ";
+   cin >> fileName;
+   
+   inputFile.open(fileName);
+   if (!inputFile) {
+      cout << "Error: Could not open file.\n";
+   }
+   else 
+   {
+      while(getline(inputFile, fileContent)) {
+         size_t posPlus = 0, posEqual=0;
+         posPlus = fileContent.find("+");
+         posEqual = fileContent.find("=");
+         strR = fileContent.substr(0, posPlus);
+         strL = fileContent.substr(posPlus + 1, (posEqual - 1) - posPlus);
+         strJ = fileContent.substr(posEqual + 1, posEqual - fileContent.length());
+
+         if (strR == "R") {
+            intR = stoi(strJ) - stoi(strL);
+            cout << intR << endl;
+         } else if (strL == "L") {
+            intL = stoi(strJ) - stoi(strR);
+            cout << intL << endl;
+         } else if (strJ == "J") {
+            intJ = stoi(strR) + stoi(strL);
+            cout << intJ << endl;
+         }
+      }
+   }
 
    return 0;
 }
