3db237b3cbfc7ecfb3a5edbc17b591e591df6c38
Chris Holcomb <chris@pooldashboard.com>
2019-10-12 00:41:31 -0500

    updated comments

--- main.cpp
+++ main.cpp
@@ -39,23 +39,23 @@ int main() {
          if (pos == string::npos) {             // check for a comma in the string
             cout << "Error: No comma in string.\n";
             foundError = true;
-         } else if (count(userData.begin(),userData.end(),',') > 1) {   // make sure the isn't more
+         } else if (count(userData.begin(),userData.end(),',') > 1) {   // make sure there isn't more
             cout << "Too many commas in input\n";                       // than one comma in the string
             foundError = true;
          } else {
             size_t posComma = userData.find(',');
             strDataPoint1 = userData.substr(0, posComma);
             strDataPoint2 = userData.substr(strDataPoint1.length() + 1, userData.length() - posComma);
-            try {
-               intDataPoint = stoi(strDataPoint2);
+            try {                                     // try to convert the second datapoint to an int
+               intDataPoint = stoi(strDataPoint2);    // if it fails, then they didn't enter an int
             } catch (exception e) {
                cout << "Error: Comma not followed by an integer.\n";
                foundError = true;
             }
          }
 
-         if (!foundError) {
-            cout << "Data string: " << strDataPoint1 << endl;
+         if (!foundError) {                                       // if we had no errors, then add the values
+            cout << "Data string: " << strDataPoint1 << endl;     // to the vectors
             cout << "Data integer: " << intDataPoint << endl;
             strDataPoints.push_back(strDataPoint1);
             intDataPoints.push_back(intDataPoint);
@@ -63,17 +63,18 @@ int main() {
       }
    }
 
+   // show formatted table
    cout << setw(33) << right << dataTitle << endl;
    cout << setw(20) << left << headerColumn1 << "|";
    cout << setw(23) << right << headerColumn2 << endl;
    cout << setfill('-') << setw(43) <<  "" << setfill(' ') << endl;
-
    for (int i=0; i < strDataPoints.size(); i++) {
       cout << setw(20) << left << strDataPoints[i] << "|";
       cout << setw(23) << right << intDataPoints[i] << endl;
    }
-
    cout << "\n\n";   
+
+   // show formatted histogram
    for (int i=0; i < strDataPoints.size(); i++) {
       cout << setw(20) << right << strDataPoints[i] << " ";
       for (int j=0; j < intDataPoints[i]; j++) {
@@ -81,6 +82,7 @@ int main() {
       }
       cout << endl;
    }
+   cout << endl;
 
    return 0;
 }
