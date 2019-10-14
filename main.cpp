94ca54e4cd5454a4d4c6d0c23f99009bd1b83f90
Chris Holcomb <chris@pooldashboard.com>
2019-10-13 19:44:02 -0500

    put it in an endless repeat.

--- main.cpp
+++ main.cpp
@@ -5,24 +5,27 @@ using namespace std;
 int main() {
    string userInput, strR, strL, strJ;
    int intR, intL, intJ;
+   bool stop=false;
 
-   getline(cin, userInput);
-   size_t posPlus=0, posEqual=0;
-   posPlus = userInput.find("+");
-   posEqual = userInput.find("=");
-   strR = userInput.substr(0, posPlus);
-   strL = userInput.substr(posPlus + 1, (posEqual - 1) - posPlus);
-   strJ = userInput.substr(posEqual + 1, posEqual - userInput.length());
+   while(!stop) {
+      getline(cin, userInput);
+      size_t posPlus=0, posEqual=0;
+      posPlus = userInput.find("+");
+      posEqual = userInput.find("=");
+      strR = userInput.substr(0, posPlus);
+      strL = userInput.substr(posPlus + 1, (posEqual - 1) - posPlus);
+      strJ = userInput.substr(posEqual + 1, posEqual - userInput.length());
 
-   if (strR == "R") {
-      intR = stoi(strJ) - stoi(strL);
-      cout << intR << endl;
-   } else if (strL == "L") {
-      intL = stoi(strJ) - stoi(strR);
-      cout << intL << endl;
-   } else if (strJ == "J") {
-      intJ = stoi(strR) + stoi(strL);
-      cout << intJ << endl;
+      if (strR == "R") {
+         intR = stoi(strJ) - stoi(strL);
+         cout << intR << endl;
+      } else if (strL == "L") {
+         intL = stoi(strJ) - stoi(strR);
+         cout << intL << endl;
+      } else if (strJ == "J") {
+         intJ = stoi(strR) + stoi(strL);
+         cout << intJ << endl;
+      }
    }
 
    return 0;
