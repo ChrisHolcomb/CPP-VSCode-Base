0c7854e8c288ef55e98065b6346c266dfe2e3a24
Chris Holcomb <chris@pooldashboard.com>
2019-10-14 19:49:24 -0500

    check user input for "EOF" or null

--- main.cpp
+++ main.cpp
@@ -5,27 +5,32 @@ using namespace std;
 int main() {
    string userInput, strR, strL, strJ;
    int intR, intL, intJ;
-   bool stop=false;
 
-   while(!stop) {
-      getline(cin, userInput);
-      size_t posPlus=0, posEqual=0;
-      posPlus = userInput.find("+");
-      posEqual = userInput.find("=");
-      strR = userInput.substr(0, posPlus);
-      strL = userInput.substr(posPlus + 1, (posEqual - 1) - posPlus);
-      strJ = userInput.substr(posEqual + 1, posEqual - userInput.length());
-
-      if (strR == "R") {
-         intR = stoi(strJ) - stoi(strL);
-         cout << intR << endl;
-      } else if (strL == "L") {
-         intL = stoi(strJ) - stoi(strR);
-         cout << intL << endl;
-      } else if (strJ == "J") {
-         intJ = stoi(strR) + stoi(strL);
-         cout << intJ << endl;
+   while(1) {
+      if (userInput == "EOF" || userInput.length() < 1) {
+         exit(1);
       }
+      else
+      {
+         getline(cin, userInput);
+         size_t posPlus=0, posEqual=0;
+         posPlus = userInput.find("+");
+         posEqual = userInput.find("=");
+         strR = userInput.substr(0, posPlus);
+         strL = userInput.substr(posPlus + 1, (posEqual - 1) - posPlus);
+         strJ = userInput.substr(posEqual + 1, posEqual - userInput.length());
+
+         if (strR == "R") {
+            intR = stoi(strJ) - stoi(strL);
+            cout << intR << endl;
+         } else if (strL == "L") {
+            intL = stoi(strJ) - stoi(strR);
+            cout << intL << endl;
+         } else if (strJ == "J") {
+            intJ = stoi(strR) + stoi(strL);
+            cout << intJ << endl;
+         }
+      }      
    }
 
    return 0;
