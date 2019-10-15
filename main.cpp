9e2fe17e8e4c19a439cdfa72b246712301a7881d
Chris Holcomb <chris@pooldashboard.com>
2019-10-15 18:33:11 -0500

    start of program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
+   vector<char> vConsonants {'p','k','h','l','m','n'};
+   string userInput;
+   bool hawaiian=false;
 
-   printf("Hello World\n");
+   cout << "Enter a Hawaiian word: ";
+   getline(cin, userInput);
+
+   for (int i=0; i<userInput.length(); i++) {
+      if (find(vConsonants.begin(), vConsonants.end(), userInput[i]) != vConsonants.end()){
+         hawaiian = true;
+      }
+   }
+
+   string isHawaiian = hawaiian ? "Yes" : "No";
+   cout << "Hawaiian Word: " << isHawaiian;
+   cout << endl;
 
    return 0;
 }
