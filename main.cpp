847f6c427f1874529aa68b7765247dc73cb7516e
Chris Holcomb <chris@pooldashboard.com>
2019-10-15 19:41:57 -0500

    now showing prononciation for vowels

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <array>
 #include <algorithm>
 using namespace std;
 
 int main() {
-   vector<char> vConsonants {'p','k','h','l','m','n'};
+   vector<char> constants {'p','k','h','l','m','n'};
+   vector<vector<string>> vowels {
+         vector<string>{"a","ah"},
+         vector<string>{"e","eh"},
+         vector<string>{"i","ee"},
+         vector<string>{"o","oh"},
+         vector<string>{"u","oo"}
+      }; 
    string userInput;
    bool hawaiian=false;
 
@@ -13,10 +21,17 @@ int main() {
    getline(cin, userInput);
 
    for (int i=0; i<userInput.length(); i++) {
-      if (find(vConsonants.begin(), vConsonants.end(), userInput[i]) != vConsonants.end()){
+      if (find(constants.begin(), constants.end(), userInput[i]) != constants.end()){
          hawaiian = true;
       }
+
+      for (int j=0; j<vowels.size(); j++) {
+         if (userInput[i] == vowels[j][0].at(0)) {
+               cout << "replacing: " << userInput[i] << " with " << vowels[j][1] << "\n";
+         }
+      }
    }
+
 
    string isHawaiian = hawaiian ? "Yes" : "No";
    cout << "Hawaiian Word: " << isHawaiian;
