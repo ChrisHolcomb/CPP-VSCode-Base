ad119340f8b172eca3891fef6a13e7454698cbc4
Chris Holcomb <chris@pooldashboard.com>
2019-10-17 23:44:35 -0500

    fixed typo

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main() {
    int numCases, numFib;
-   string result = "";
    vector<int> fibNumbers;
 
    cin >> numCases;
@@ -21,13 +20,13 @@ int main() {
       fib[0] = 0; 
       fib[1] = 1; 
 
-      for (int i = 2; i <= numFib; i++) 
+      for (int i = 2; i <= calcFibNum; i++) 
       { 
          fib[i] = fib[i-1] + fib[i-2]; 
       }
       cout << setprecision(20);
-      cout << "Case " << (testCase + 1) <<": fib(" << calcFibNum << ") = ";
-      cout << fib[calcFibNum] << "\n";
+      cout << "Case " << (testCase + 1) << ": fib(" << calcFibNum << ") = ";
+      cout << fib[calcFibNum] << endl;
    }
    
    return 0;
