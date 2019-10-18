678aa591adb3ee7733d72494c6df1b63ca0cbf05
Chris Holcomb <chris@pooldashboard.com>
2019-10-17 23:07:38 -0500

    corrected to not try and calculate if fib is 94+

--- main.cpp
+++ main.cpp
@@ -15,12 +15,12 @@ int main() {
       fib[0] = 0; 
       fib[1] = 1; 
 
-      for (int i = 2; i <= numFib; i++) 
-      { 
-         fib[i] = fib[i-1] + fib[i-2]; 
-      }
       string strFibNum;
       if (numFib < 94) {
+         for (int i = 2; i <= numFib; i++) 
+         { 
+            fib[i] = fib[i-1] + fib[i-2]; 
+         }
          strFibNum = to_string(fib[numFib]);
       } else if (numFib == 94) {
          strFibNum = "19740274219868223167";      
