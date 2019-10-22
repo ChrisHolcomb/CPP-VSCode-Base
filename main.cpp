0c35483e9eeb7caa574d3bb6c6a94c28bbf24346
Chris Holcomb <chris@pooldashboard.com>
2019-10-21 19:49:33 -0500

    printint array of 1s

--- main.cpp
+++ main.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <vector>
+#include <array>
+
+
 using namespace std;
 
-int main() {
 
-   printf("Hello World\n");
+void printArray(int n) {
+   int myArray[n][n];
+
+   // Build it
+   // Row
+   for (int i=0; i < n; i++) {
+      // Column
+      for (int j=0; j < n; j++) {
+         myArray[i][j] = 1;
+      }
+   }
+
+   // Print it
+   // Row
+   for (int i=0; i < n; i++) {
+      // Column
+      for (int j=0; j < n; j++) {
+         cout << setw(3) << right << myArray[i][j];
+      }
+      cout << "\n";
+   }
+   cout << "\n";
+}
+
+int main() {
+   vector<int> userInputArray;
+   int userInput = -1;
+
+   while (userInput != 0) {
+      cin >> userInput;
+      if (userInput == 0)
+      {
+         break;
+      }
+      else {
+         userInputArray.push_back(userInput);
+      }
+   }
+
+   for_each(userInputArray.begin(), userInputArray.end(), printArray);
+   cout << "\n";
 
    return 0;
 }
