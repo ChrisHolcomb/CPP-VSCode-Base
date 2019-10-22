67970426fb14881cfab73507bbc3b2d1d1a75076
Chris Holcomb <chris@pooldashboard.com>
2019-10-21 20:45:50 -0500

    working up to 5

--- main.cpp
+++ main.cpp
@@ -9,19 +9,30 @@ void printArray(int n) {
 
    // Build it
    // Row
-   for (int i=0; i < n; i++) {
+   for (int r=0; r < n; r++) {
       // Column
-      for (int j=0; j < n; j++) {
-         myArray[i][j] = 1;
+      for (int c=0; c < n; c++) {
+         int y = 0;
+         if (r == 0 || c == 0 || r + 1 == n || c + 1 == n) {
+            y = 1;
+         } else if (r == c && r + c == n -1) {
+            y = 3;
+         } else {
+            y = 2;
+         }
+         myArray[r][c] = y;
       }
    }
 
    // Print it
    // Row
-   for (int i=0; i < n; i++) {
+   for (int r=0; r < n; r++) {
       // Column
-      for (int j=0; j < n; j++) {
-         cout << setw(3) << right << myArray[i][j];
+      for (int c=0; c < n; c++) {
+         cout << setw(3) << right << myArray[r][c];
+         if (c + 1 < n) {
+            cout << " ";
+         }
       }
       cout << "\n";
    }
