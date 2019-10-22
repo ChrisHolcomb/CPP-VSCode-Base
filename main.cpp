38acb4d7244713cb8bc8bdd19a518942a4a332da
Chris Holcomb <chris@pooldashboard.com>
2019-10-21 21:13:54 -0500

    without seperate function

--- main.cpp
+++ main.cpp
@@ -5,40 +5,6 @@
 
 using namespace std;
 
-void printArray(int n) {
-   int myArray[n][n];
-
-   // Build it
-   for (int i = 1; i <= ceil(n / 2.0); i++) {
-      for (int j = i-1; j < n-i; j++) {
-         myArray[i-1][j] = i;
-      }
-      for (int k = i-1; k <= n-i; k++) {
-         myArray[k][n - i] = i;
-      }
-      for (int x = n-i; x >= i-1; x--) {
-         myArray[n-i][x] = i;
-      }
-      for (int y = n-i; y >= i-1; y--) {
-         myArray[y][i - 1] = i;
-      }
-   }
-
-   // Print it
-   // Row
-   for (int r=0; r < n; r++) {
-      // Column
-      for (int c=0; c < n; c++) {
-         cout << setw(3) << right << myArray[r][c];
-         if (c + 1 < n) {
-            cout << " ";
-         }
-      }
-      cout << "\n";
-   }
-   cout << "\n";
-}
-
 int main() {
    vector<int> userInputArray;
    int userInput = -1;
@@ -55,7 +21,39 @@ int main() {
    }
 
    for (int i=0; i < userInputArray.size(); i++) {
-      printArray(userInputArray[i]);
+      //printArray(userInputArray[i]);
+      int n = userInputArray[i];
+      int myArray[n][n];
+
+      // Build it
+      for (int i = 1; i <= ceil(n / 2.0); i++) {
+         for (int j = i-1; j < n-i; j++) {
+            myArray[i-1][j] = i;
+         }
+         for (int k = i-1; k <= n-i; k++) {
+            myArray[k][n - i] = i;
+         }
+         for (int x = n-i; x >= i-1; x--) {
+            myArray[n-i][x] = i;
+         }
+         for (int y = n-i; y >= i-1; y--) {
+            myArray[y][i - 1] = i;
+         }
+      }
+
+      // Print it
+      // Row
+      for (int r=0; r < n; r++) {
+         // Column
+         for (int c=0; c < n; c++) {
+            cout << setw(3) << right << myArray[r][c];
+            if (c + 1 < n) {
+               cout << " ";
+            }
+         }
+         cout << "\n";
+      }
+      cout << "\n";
    }
    cout << "\n";
 
