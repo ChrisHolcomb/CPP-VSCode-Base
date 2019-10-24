211c40bd23796ea60985269155364f80824a95fb
Chris Holcomb <chris@pooldashboard.com>
2019-10-23 19:45:56 -0500

    intial setup

--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int main() {
+   string inputString;
+   int matrixSize=0, inputReadCount=0;
 
-   printf("Hello World\n");
+   // get our input string
+   getline(cin, inputString);
+   // convert string to a charactor array
+   vector<char> inputMessage(inputString.begin(), inputString.end());
+   // get our matrix square size
+   // TODO: come back and make sure this is an odd square
+   matrixSize = ceil(sqrt(inputMessage.size()));
+   // set our messageArray
+   char messageArray[matrixSize][matrixSize];
+
+   // fill our message array
+   for (int i=0; i < matrixSize; i++) {
+      for (int j=0; j < matrixSize; j++) {
+         if (inputReadCount < inputMessage.size()) {
+            messageArray[i][j] = inputMessage[inputReadCount];
+         } else {
+            messageArray[i][j] = '*';
+         }
+         inputReadCount++;
+      }
+   }
+
+   // TESTING - print out to test results of matrix
+   for (int i=0; i < matrixSize; i++) {
+      for (int j=0; j < matrixSize; j++) {
+         cout << messageArray[i][j] << " ";
+      }
+      cout << "\n";
+   }
+   cout << "\n";
 
    return 0;
 }
