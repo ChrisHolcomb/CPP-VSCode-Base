c5148f9a811d84690a5e460c6a385343fe1c0d79
Chris Holcomb <chris@pooldashboard.com>
2019-10-26 22:49:02 -0500

    add for second part with *s

--- main.cpp
+++ main.cpp
@@ -4,16 +4,29 @@ using namespace std;
 
 int main() {
     int rows, num=0;
-    string results = "";
 
     cout << "Enter number of rows: ";
     cin >> rows;
 
-    for (int i = 0; i < rows * 2; i += 2) {
-        for (int j = 0; j <= i; j += 2) {
-            cout << j << " ";
+    // for (int i = 0; i < rows * 2; i += 2) {
+    //     for (int j = 0; j <= i; j += 2) {
+    //         cout << j << " ";
+    //     }
+    //     cout << "\n";
+    // }
+    // cout << "\n";
+
+    for (int i = 1; i <= rows; i++) {
+        // print spaces
+        for (int j = 0; j < i - 1; j++) {
+            cout << " ";
+        }
+        // print "*"
+        for (int j = 0; j < i; j++) {
+            cout << "*";
         }
         cout << "\n";
+
     }
     cout << "\n";
 
