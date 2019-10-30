e659e9ebe5411243ef524251bc08619319723f03
Chris Holcomb <chris@pooldashboard.com>
2019-10-29 20:07:45 -0500

    dynamic matrix created from file

--- main.cpp
+++ main.cpp
@@ -1,112 +1,66 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-int main() {
-    ifstream file1, file2;
-    string fileName1, fileName2;
-    string operation;
-    int r1, c1, r2, c2, nums;
-    int matrix2[r2][c2];
-    int matrix1[r1][c1];
-    
-    getline(cin, fileName1);
-    getline(cin, fileName2);
-    getline(cin, operation);
-    
-    for (unsigned int i = 0; i < operation.size(); i++){
-        operation[i] = toupper(operation[i]);
-    }
+vector<vector<int>> getArrayForFile(string fileName) {
+   ifstream myFile;
+   string line;
+   vector<vector<int>> myVector;
+   int r=0, c=0, n=0;
+   istringstream iss;
 
-    
-    file1.open(fileName1);
-    if (!file1) {
-        cout << "Error: File " << fileName1 << " did not open." << endl;
-    }
-    else {
-        while (!file1.eof()) {
-            cin >> r1;
-            cin >> c1;
-            int matrixNums;
-            matrixNums = r1 * c1;
-            int numCount = 0;
-            while (numCount <= matrixNums) {
-                for (int i = 0; i < r1; i++) {
-                    for (int j = 0; j < c1; j++) {
-                        cin >> nums;
-                        matrix1[i][j] = nums;
-                        numCount++;
-                    }
-                }
-            }
-        }
-    }
-    
-    file2.open(fileName2);
-    if (!file2) {
-        cout << "Error: File " << fileName2 << " did not open." << endl;
-    }
-    else {
-        while (!file2.eof()) {
-            cin >> r2;
-            cin >> c2;
-            
-            int matrixNums;
-            matrixNums = r2 * c2;
-            int numCount = 0;
-            while (numCount <= matrixNums){
-                for (int i = 0; i < r2; i++) {
-                    for (int j = 0; j < c2; j++) {
-                        cin >> nums;
-                        matrix2[i][j] = nums;
-                        numCount++;
-                    }
-                }
-            }
-        }
-    }
-    
-    if (r1 != r2 || c1 != c2) {
-        cout << "Two matrices are not " << operation << " comparable." << endl;
-    }
-    else {
-        int outputMatrix[r1][c2];
-        if (operation == "add") {
-            for (int i = 0; i < r1; i++) {
-                for (int j = 0; j < c1; j++) {
-                    outputMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
-                    cout << outputMatrix[i][j] << " ";
-                }
-                cout << endl;
-            }
-        }
-        else if (operation == "subtract") {
-            for (int i = 0; i < r1; i++) {
-                for (int j = 0; j < c1; j++) {
-                    outputMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
-                    cout << outputMatrix[i][j] << " ";
-                }
-                cout << endl;
-            }
-        }
-        else if (operation == "multiply") {
-            for (int i = 0; i < r1; i++) {
-                for (int j = 0; j < c1; j++) {
-                    outputMatrix[i][j] = matrix1[i][j] * matrix2[i][j];
-                    cout << outputMatrix[i][j] << " ";
-                }
-                cout << endl;
-            }
-        }
-    }
-    
-    
-    
-    
-    
-    file1.close();
-    file2.close();
-    return 0;
+   myFile.open(fileName);
+   if (!myFile){
+      cout << "Could not open file " << fileName << "\n";
+      return myVector;
+   }
+   getline(myFile, line);
+   iss.str(line);
+   iss >> r >> c;
+   for (int i=0; i < r; i++) {
+      vector<int> tmp;
+      for (int j=0; j < c; j++) {
+         getline(myFile, line);
+         iss.clear();
+         iss.str(line);
+         iss >> n;
+         tmp.push_back(n);
+      }
+      myVector.push_back(tmp);
+   }
+
+   return myVector;
+}
+
+void showMatrix(vector<vector<int>> myVector) {
+   for (int i=0; i < (int)myVector.size(); i++) {
+      for (int j=0; j < (int)myVector[i].size(); j++) {
+         cout << myVector[i][j] << " ";
+      }
+      cout << "\n";
+   }
+   cout << "\n";
+}
+
+int main() {
+   string fileName1, fileName2;
+   vector<vector<int>> matrix1Array;
+   vector<vector<int>> matrix2Array;
+
+   cout << "Enter file 1 name: ";
+   cin >> fileName1;
+   cout << "Enter file 2 name: ";
+   cin >> fileName2;
+
+   matrix1Array = getArrayForFile(fileName1);
+   matrix2Array = getArrayForFile(fileName2);
+
+   showMatrix(matrix1Array);   
+   showMatrix(matrix2Array);   
+   
+   return 0;
 }
