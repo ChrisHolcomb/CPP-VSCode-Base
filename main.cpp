f600609e4997b565322ba2dd78baf254578bcf42
Chris Holcomb <chris@pooldashboard.com>
2019-10-30 13:55:13 -0500

    added more debug code

--- main.cpp
+++ main.cpp
@@ -50,18 +50,17 @@ void showMatrix(vector<vector<int>> myVector) {
 
 vector<vector<int>> outWithOperator(vector<vector<int>> matrix1, vector<vector<int>> matrix2, string myOperator) {
    vector<vector<int>> outputVector;
-
    if (matrix1.size() != matrix2.size()) {
-      cout << "NOT COMPATIBLE\n";
+      cout << "Two matrices are not " << myOperator << " comparable.\n";
       return outputVector;
    }
 
    for (int i=0; i < (int)matrix1.size(); i++) {
       vector<int> tmp;
       for (int j=0; j < (int)matrix1[i].size(); j++) {
-         if (myOperator == "add") tmp.push_back(matrix1[i][j] + matrix2[i][j]);
-         if (myOperator == "subtract") tmp.push_back(matrix1[i][j] - matrix2[i][j]);
-         if (myOperator == "multiply") tmp.push_back(matrix1[i][j] * matrix2[i][j]);         
+         if (myOperator == "ADD") tmp.push_back(matrix1[i][j] + matrix2[i][j]);
+         if (myOperator == "SUBTRACT") tmp.push_back(matrix1[i][j] - matrix2[i][j]);
+         if (myOperator == "MULTIPLY") tmp.push_back(matrix1[i][j] * matrix2[i][j]);         
       }
       outputVector.push_back(tmp);
    }
@@ -69,8 +68,24 @@ vector<vector<int>> outWithOperator(vector<vector<int>> matrix1, vector<vector<i
    return outputVector;
 }
 
+// For debuging data file
+void showDataFile(string fileName) {
+   ifstream myFile;
+   string line;
+
+   myFile.open(fileName);
+   if (!myFile){
+      cout << "Could not open file " << fileName << "\n";
+   }
+   while(getline(myFile, line)) {
+      cout << line << "\n";
+   }
+
+   myFile.close();
+}
+
 int main() {
-   string fileName1, fileName2;
+   string fileName1, fileName2, operation;
    vector<vector<int>> matrix1Array;
    vector<vector<int>> matrix2Array;
    vector<vector<int>> outputArray;
@@ -79,22 +94,36 @@ int main() {
    cin >> fileName1;
    cout << "Enter file 2 name: ";
    cin >> fileName2;
+   cout << "Enter operator: ";
+   cin >> operation;
+
+   for (int w = 0; w < (int)operation.length(); w++) {
+      operation[w] = toupper(operation[w]);
+   }
 
    matrix1Array = getArrayForFile(fileName1);
    matrix2Array = getArrayForFile(fileName2);
 
-   showMatrix(matrix1Array);   
-   showMatrix(matrix2Array);
+   outputArray = outWithOperator(matrix1Array, matrix2Array, operation);
+   showMatrix(outputArray);
 
-   outputArray = outWithOperator(matrix1Array, matrix2Array, "add");
-   cout << "ADD" << "\n";
-   showMatrix(outputArray);
-   outputArray = outWithOperator(matrix1Array, matrix2Array, "subtract");
-   cout << "SUBTRACT" << "\n";
-   showMatrix(outputArray);
-   outputArray = outWithOperator(matrix1Array, matrix2Array, "multiply");
-   cout << "MULTIPLY" << "\n";
-   showMatrix(outputArray);
+   // DEBUG - Show matrix for file 1 and 2
+   // showMatrix(matrix1Array);   
+   // showMatrix(matrix2Array);
+
+   // DEBUG - Show format of input file
+   //showDataFile(fileName1);
+
+   // DEBUG - Test restuls for each operator
+   // outputArray = outWithOperator(matrix1Array, matrix2Array, "ADD");
+   // cout << "ADD" << "\n";
+   // showMatrix(outputArray);
+   // outputArray = outWithOperator(matrix1Array, matrix2Array, "SUBTRACT");
+   // cout << "SUBTRACT" << "\n";
+   // showMatrix(outputArray);
+   // outputArray = outWithOperator(matrix1Array, matrix2Array, "MULTIPLY");
+   // cout << "MULTIPLY" << "\n";
+   // showMatrix(outputArray);
    
    return 0;
 }
