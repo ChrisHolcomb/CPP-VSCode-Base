99629babf73d6a772a794b7a7ea187ecbc0efc12
Chris Holcomb <chris@pooldashboard.com>
2019-11-04 21:07:58 -0600

    fixed specification file

--- myCustomCalc.h
+++ myCustomCalc.h
@@ -1,8 +1,8 @@
 #ifndef MYCUSTOMCALC_H
 #define MYCUSTOMCALC_H
 
-int add(int a, int b);
-int subtract(int a, int b) ;
-int mulitply(int a, int b) ;
+int add(int, int);
+int subtract(int, int) ;
+int mulitply(int, int) ;
 
 #endif
