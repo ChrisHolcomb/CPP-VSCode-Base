7d2ee94f353b90fdcd64904123b1105a89c02ec3
Chris Holcomb <chris@pooldashboard.com>
2019-11-04 20:40:18 -0600

    working header file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-#include <string>
+#include "myCustomCalc.h"
 using namespace std;
 
 int main() {
 
-   printf("Hello World\n");
+   int a, b, result;
+   string operation;
+
+   cout << "Enter 2 integers: ";
+   cin >> a >> b;
+   cout << "Enter an operator (add/subtract/multiply): ";
+   cin >> operation;
+
+   if (operation == "add") {
+      result = add(a,b);
+   } else if (operation == "subtract") {
+      result = subtract(a,b);
+   } else if (operation == "multiply") {
+      result = mulitply(a,b);
+   } else {
+      cout << "wrong operator.";
+      return -1;
+   }
+
+   cout << "Result: " << result << "\n";
 
    return 0;
 }
---
+++ myCustomCalc.h
@@ -0,0 +1,16 @@
+#ifndef MyCustomCalc
+#define MyCustomCalc
+
+int add(int a, int b) {
+    return a+b;
+}
+
+int subtract(int a, int b) {
+    return a-b;
+}
+
+int mulitply(int a, int b) {
+    return a*b;
+}
+
+#endif
