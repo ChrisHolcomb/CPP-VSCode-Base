17dc52f552a1b08a27d5dd7dd5643f465b7555b5
Chris Holcomb <chris@pooldashboard.com>
2019-11-07 18:08:01 -0600

    working

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-#include <string>
+#include <vector>
+
 using namespace std;
 
-int main() {
+int GetFrequencyOfWord(vector<string> wordList, string currWord) {
+   int count=0;
 
-   printf("Hello World\n");
+   for (int i=0; i < (int)wordList.size(); i++) {
+      if (wordList[i] == currWord) {
+         count++;
+      }
+   }
+
+   return count;
+}
+
+int main() {
+   int numWord=0, wordCount=0;
+   string word;
+   vector<string> wordArray;
+
+   cin >> numWord;
+   for (int i=0; i < numWord; i++) {
+      cin >> word;
+      wordArray.push_back(word);
+   }
+
+   for (int i=0; i < (int)wordArray.size(); i++) {
+      cout << wordArray[i] << " " << GetFrequencyOfWord(wordArray, wordArray[i]) << endl;
+   }
+
+   cout << endl;
 
    return 0;
 }
