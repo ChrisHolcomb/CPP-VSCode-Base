b0efaf3bf9f2f2be56a50be7ee4de76cbd3f1d10
Chris Holcomb <chris@pooldashboard.com>
2019-11-07 21:53:11 -0600

    more cleanup

--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 #include <fstream>
-#include "myFunctions.h"
 #include <iomanip>
 #include <sstream>
+#include "myFunctions.h"
 
 using namespace std;
 
-const int WIDTH = 5;
+string CreateTriangle(int, char);
+void CreateTriangleLine(ostringstream&, int, int);
+const int WIDTH = 5; 
 
-void CreateTriangleLine(ostringstream &oss, int numRows, int i) {
-   // Print the Spaces before the numbers
-   for (int space = 1; space < numRows - i; space++) {
-      oss <<  setw(WIDTH) << " ";
-   }
-   // Print the number
-   for (int j = 0; j <= i; j++) {
-      oss << setw(WIDTH) << right << combination(i, j);
-      oss << setw(WIDTH) << " ";
-   }
-   oss << endl;
+int main() {
+   int numRows;
+   ofstream outFS;
+   string triangleString;
+   
+   cout << "Please Enter The Number of Rows: ";
+   cin >> numRows;
+   cout << numRows << endl;
+   
+   // Create the Triangle
+   triangleString = CreateTriangle(numRows, 'U');
+   cout << triangleString;
+   // Open the file, output the results to the file and close it
+   outFS.open("Pascal.txt");
+   outFS << triangleString;
+   outFS.close();
+   
+   // Extra Credit Section
+   cout << endl;
+   cout << "*** Start Extra Credit ***" << endl;
+   cout << endl;
+   
+   cout << CreateTriangle(numRows, 'D');
+   
+   cout << endl;
+   cout << "*** End Extra Credit ***" << endl;
+
+   return 0;
 }
 
 string CreateTriangle(int numRows, char direction) {
@@ -42,34 +61,15 @@ string CreateTriangle(int numRows, char direction) {
    return oss.str();
 }
 
-int main() {
-   //combinations
-   //use functions from wednesday's hw
-   int numRows;
-   ofstream outFS;
-   string triangleString;
-   
-   cout << "Please Enter The Number of Rows: ";
-   cin >> numRows;
-   cout << numRows << endl;
-   cout << endl;
-   
-   // Create the Triangle
-   triangleString = CreateTriangle(numRows, 'U');
-   cout << triangleString;
-   // Open the file, output the results to the file and close it
-   outFS.open("Pascal.txt");
-   outFS << triangleString;
-   outFS.close();
-   
-   cout << endl;
-   cout << "*** Start Extra Credit ***" << endl;
-   cout << endl;
-   
-   cout << CreateTriangle(numRows, 'D');
-   
-   cout << endl;
-   cout << "*** End Extra Credit ***" << endl;
-
-   return 0;
+void CreateTriangleLine(ostringstream &oss, int numRows, int i) {
+   // Print the Spaces before the numbers
+   for (int space = 1; space < numRows - i; space++) {
+      oss <<  setw(WIDTH) << " ";
+   }
+   // Print the number
+   for (int j = 0; j <= i; j++) {
+      oss << setw(WIDTH) << right << combination(i, j);
+      oss << setw(WIDTH) << " ";
+   }
+   oss << endl;
 }
