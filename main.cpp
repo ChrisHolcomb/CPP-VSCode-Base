e72cbd7e53226965c8adf08703487b41ef69af62
Chris Holcomb <chris@pooldashboard.com>
2019-11-10 19:09:29 -0600

    updated test

--- main.cpp
+++ main.cpp
@@ -2,19 +2,43 @@
 #include "sphere.h"
 using namespace std;
 
+void PrintSphere(Sphere sphere);
+
 int main() {
    Sphere sphere1;
    Sphere sphere2(2,"blue");
 
-   cout << "Sphere1 radius: " << sphere1.getRadius();
-   cout << ", Sphere1 Color: " << sphere1.getColor();
-   cout << endl;
-   cout << "Sphere2 radius: " << sphere2.getRadius();
-   cout << ", Sphere2 Color: " << sphere2.getColor();
-   cout << endl;
+   cout << "*** SPHERE 1 ***\n";
+   PrintSphere(sphere1);
+   
+   cout << "*** SPHERE 2 ***\n";
+   PrintSphere(sphere2);
+
    cout << "Spheres are equal: " << (sphere1.isEqual(sphere2) ? "Yes" : "No");
+   cout << endl;
+
+   cout << "\nModifing Sphere 1\n\n";
+   sphere1.setRadius(2);
+   sphere1.setColor("blue");
+
+   cout << "*** SPHERE 1 ***\n";
+   PrintSphere(sphere1);
+   
+   cout << "*** SPHERE 2 ***\n";
+   PrintSphere(sphere2);
+
+   cout << "Spheres are equal: " << (sphere1.isEqual(sphere2) ? "Yes" : "No");
+   cout << endl;
 
    cout << endl;
 
    return 0;
+}
+
+void PrintSphere(Sphere sphere)
+{
+   cout << "Radius: " << sphere.getRadius();
+   cout << "\nColor: " << sphere.getColor();
+   cout << endl;
+
 }
