969d6f3bc3afaa3b89b47b0d5ec6c4f80dad7e1c
Chris Holcomb <chris@pooldashboard.com>
2019-12-03 21:30:29 -0600

    clean up

--- main.cpp
+++ main.cpp
@@ -5,6 +5,41 @@
 
 using namespace std;
 
+void FindPair(string* d, int* count, string pair, int arraySize);
+void SortPair(string* d, int* count, int arraySize);
+void DisplayResults(string d[], int count[], string testContent);
+void ReadInput(int numLines);
+
+int main() {
+    int numLines = -1;
+    
+    while (numLines != 0) {
+        cin >> numLines;
+        cin.ignore();
+        if (numLines > 0){
+            ReadInput(numLines);
+        }
+    } 
+
+    return 0;
+}
+
+void ReadInput(int numLines) {
+    const int arraySize = 1000;
+    string d[arraySize];
+    int count[arraySize] = {0};
+    string lineContent, testContent;
+    for (int i = 0; i < numLines; i++) {
+        getline(cin, lineContent);
+        testContent += lineContent;
+    }
+    for (unsigned int i = 0; i < (testContent.length() - 1); i++) {
+        FindPair(d, count, testContent.substr(i, 2), arraySize);
+    }
+    SortPair(d, count, arraySize);
+    DisplayResults(d, count, testContent);
+}
+
 void FindPair(string* d, int* count, string pair, int arraySize) {
     bool found = false;
     
@@ -55,36 +90,4 @@ void DisplayResults(string d[], int count[], string testContent) {
         cout << d[i] << " " << count[i] << " "
         << fixed << setprecision(6) << frequency << endl;
     }
-}
-
-void ReadInput(int numLines) {
-    const int arraySize = 1000;
-    string d[arraySize];
-    int count[arraySize] = {0};
-    string lineContent, testContent;
-    for (int i = 0; i < numLines; i++) {
-        getline(cin, lineContent);
-        testContent += lineContent;
-    }
-    for (unsigned int i = 0; i < (testContent.length() - 1); i++) {
-        FindPair(d, count, testContent.substr(i, 2), arraySize);
-    }
-    SortPair(d, count, arraySize);
-    DisplayResults(d, count, testContent);
-}
-
-int main() {
-
-    int numLines = -1;
-    
-    while (numLines != 0) {
-        cin >> numLines;
-        cin.ignore();
-        if (numLines > 0){
-            ReadInput(numLines);
-        }
-    }
-    
-    
-    return 0;
 }
